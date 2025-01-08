d071bfd889bb86cbc6bd9c15fda1385bc341ffb6
larryp89 <42215520+larryp89@users.noreply.github.com>
2025-01-08 16:56:47 +0000

    Modify directory structure

--- frontend/blog/src/Greeting.jsx
+++
@@ -1,5 +0,0 @@
-function Greeting() {
-  return <h1>"I swear by my pretty floral bonnet, I will end you."</h1>;
-}
-
-export default Greeting;
---
+++ frontend/blog/src/components/Blog.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Blog() {
+  return (
+    <>
+      <h1>First blog post</h1>
+      <h2>Second blog post</h2>
+    </>
+  );
+}
+
+export default Blog;
